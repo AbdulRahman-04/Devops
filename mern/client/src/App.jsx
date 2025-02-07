1a3de6ad2504940140a17960a83d064cf9673061
Abdul Rahman <Abdulrahman.81869@gmail.com>
2025-02-07 10:52:27 +0000

    deployment

--- mern/client/src/App.jsx
+++ mern/client/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import "./App.css"
 const App = () => {
   return (
-    <div className='text-red-900 animate-bounce text-center mt-60'>HELLO WORLD</div>
+    <div className='text-red-900 animate-bounce text-center mt-60'>HELLO WORLD <p>Lorem ipsum dolor sit amet.</p></div>
+    
+
+ 
   )
 }
 
--- mern/server/app.js
+++ mern/server/app.js
@@ -8,9 +8,9 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-const PORT = 5050;
+const PORT = 5051;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/rahman", (req, res)=>{
     try{
@@ -19,9 +19,10 @@ app.get("/rahman", (req, res)=>{
         res.status(401).json(error)
     }
  });
+ 
 
  app.get("/*", (req, res)=>{
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
  });
 
  app.listen(PORT, ()=>{
