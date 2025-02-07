8aee97a6d1f6183e07c96c70cb32b23921e9ad1e
Abdul Rahman <Abdulrahman.81869@gmail.com>
2025-02-07 10:27:58 +0000

    DEPLOYMENT

---
+++ mern/client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./App.css"
+const App = () => {
+  return (
+    <div className='text-red-900 animate-bounce text-center mt-60'>HELLO WORLD</div>
+  )
+}
+
+export default App
